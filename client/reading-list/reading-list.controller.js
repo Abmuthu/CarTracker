d81923407aac584a0d93e71e3c5b5d1e8869e9fb
Abmuthu <mp4155@nyu.edu>
2017-06-16 13:50:50 -0400

    feat(module-client): implement feature to select a specific signal

--- client/reading-list/reading-list.controller.js
+++ client/reading-list/reading-list.controller.js
@@ -6,19 +6,26 @@
 		var self = this;
 
 		self.getReadingsOfAVehicle = function() {
-
-			ReadingService.getReadingsOfAVehicle(self.vin)
+			ReadingService.getReadingsOfAVehicle(self.vin, self.timeRange)
 				.then(function (data) {
 					self.readings = data;
 				}).then(function () {
-					self.fuelVolume = [];
+					self.signal = [];
+					console.log("The selectedSignal is: " + self.selectedSignal);
 					self.readings.forEach(function(reading) {
-						self.fuelVolume.push(reading.fuelVolume);
+						if (self.selectedSignal === "fuelVolume") {
+							self.signal.push(reading.fuelVolume);
+						}	
+						else if (self.selectedSignal === "speed") {
+							self.signal.push(reading.speed);
+						}
+						// self.signal.push(reading.fuelVolume);
 					});
+					
 					$scope.myJson = {
 				    	type : 'line',
 				    	series : [
-				      	{ values : self.fuelVolume }
+				      	{ values : self.signal }
 				    	]
 					};
 				});
--- client/reading-list/reading.service.js
+++ client/reading-list/reading.service.js
@@ -3,8 +3,8 @@
 	app.service("ReadingService", function($http) {
 		var self = this;
 
-		self.getReadingsOfAVehicle = function(vin) {
-			var promise1 = $http.get("http://localhost:8081/api/readings/" + vin);
+		self.getReadingsOfAVehicle = function(vin, timeRange) {
+			var promise1 = $http.get("http://localhost:8081/api/readings/" + vin + "/" + timeRange);
 			var promise2 = promise1.then(function(response) {
 				return response.data;
 			});
